ddb7cf77284bd29c82e9e733aa69844884c096ea
Rahul Negi <rahulnegi1555@gmail.com>
2024-01-04 23:03:33 +0530

    Add files via upload

---
+++ Data Cleaning and Transformation (SQL Scripts)/FACT_InternetSales.sql
@@ -0,0 +1,33 @@
+-- Cleansed FACT_InternetSales Table --
+SELECT 
+  [ProductKey], 
+  [OrderDateKey], 
+  [DueDateKey], 
+  [ShipDateKey], 
+  [CustomerKey], 
+  --  ,[PromotionKey]
+  --  ,[CurrencyKey]
+  --  ,[SalesTerritoryKey]
+  [SalesOrderNumber], 
+  --  [SalesOrderLineNumber], 
+  --  ,[RevisionNumber]
+  --  ,[OrderQuantity], 
+  --  ,[UnitPrice], 
+  --  ,[ExtendedAmount]
+  --  ,[UnitPriceDiscountPct]
+  --  ,[DiscountAmount] 
+  --  ,[ProductStandardCost]
+  --  ,[TotalProductCost] 
+  [SalesAmount]--  ,[TaxAmt]
+  --  ,[Freight]
+  --  ,[CarrierTrackingNumber] 
+  --  ,[CustomerPONumber] 
+  --  ,[OrderDate] 
+  --  ,[DueDate] 
+  --  ,[ShipDate] 
+FROM 
+  AdventureWorksDW2022.dbo.FactInternetsales
+WHERE 
+  LEFT(OrderDateKey, 4) IN ('2020', '2021', '2022') -- Ensures we always only bring three years of data
+ORDER BY
+  OrderDateKey ASC
